c401c4b8ef61efd4435deab8c6b41c12699e9541
Steffi232 <thensteffi@gmail.com>
2025-03-27 23:46:18 +0700

    adjusting products

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -5,13 +5,7 @@
         <h5 style="color:#fff; background-color: #4B0D74; border: 2px solid; padding: 10px; border-radius: 5px;">Add New Product</h5>
         <form action="{{ route('products.store') }}" method="POST">
             @csrf
-            <div class="form-group mb-3">
-                <label for="code" style="color: #F05AE0;">Product Code</label>
-                <input type="text" name="code" id="code" class="form-control @error('code') is-invalid @enderror" value="{{ old('code') }}" required>
-                @error('code')
-                    <span class="text-danger">{{ $message }}</span>
-                @enderror
-            </div>
+
             <div class="form-group mb-3">
                 <label for="name" style="color: #F05AE0;">Product Name</label>
                 <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name') }}" required>
@@ -29,7 +23,7 @@
             <div class="form-group mb-3">
                 <label for="category" style="color: #F05AE0;">Category</label>
                 <select name="category" id="category" class="form-control @error('category') is-invalid @enderror" required>
-                    <option value="">-- Select Category --</option>
+                    <option value="">Select category</option>
                     @foreach(App\Models\Product::getCategories() as $category)
                         <option value="{{ $category }}" {{ old('category') == $category ? 'selected' : '' }}>{{ $category }}</option>
                     @endforeach
